19491f64b25270784ba474f7ed6633eb6bc0908c
Victor Mikell <78280727+vmikell@users.noreply.github.com>
2022-09-16 20:27:36 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,29 @@
+window.onscroll = function () {
+  scrollNavBar()
+}
+
+function scrollNavBar() {
+  if (window.innerWidth > 992) {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      document.querySelector('.navbar').style.height = '75px'
+      document.querySelector('#logo-img').style.maxWidth = '125px'
+    } else {
+      document.querySelector('.navbar').style.height = '139.4px'
+      document.querySelector('#logo-img').style.maxWidth = '200px'
+    }
+  } else {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      document.querySelector('.navbar').style.height = '75px'
+      document.querySelector('.logo-img-mobile').style.height = '50px'
+    } else {
+      document.querySelector('.navbar').style.height = '118.55px'
+      document.querySelector('.logo-img-mobile').style.height = '92.55px'
+    }
+  }
+}
